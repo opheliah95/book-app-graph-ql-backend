4cc3807c328c92366f8d1edeba34cfad5db9642c
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-10-02 14:04:49 +0100

    imported schema

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express()
 var { createHandler }=  require("graphql-http/lib/use/express")
 var { buildSchema } = require("graphql")
 
-
+const schema = require("./schema/schema")
 app.use("/graphql", createHandler({
-    
+    schema: schema
 }))
 app.listen(4000, () => {
     console.log("listening request on port 4000")
