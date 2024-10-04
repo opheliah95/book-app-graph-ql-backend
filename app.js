aa5238121d68f9aeb9dc3b98229bbbcd46a23b88
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-10-04 14:27:05 +0100

    added graphic interface

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const app = express()
 var { createHandler }=  require("graphql-http/lib/use/express")
-
+var { ruruHTML } = require("ruru/server")
+ 
 const schema = require("./schema/schema")
 
-app.get("/", (req, res) => {
-    return res.json({message: "Welcome to the graphql backend"})
-})
-
+app.get("/", (_req, res) => {
+    res.type("html")
+    res.end(ruruHTML({ endpoint: "/graphql" }))
+  })
+  
 app.use("/graphql", createHandler({
     schema: schema
 }))
