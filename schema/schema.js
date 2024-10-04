9d3bc80c285203acba0506d6ee317b3e318c8d06
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-10-04 14:36:54 +0100

    udpated string to graphql id

--- schema/schema.js
+++ schema/schema.js
@@ -1,9 +1,9 @@
 const graphql = require("graphql")
 const _ = require("lodash")
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 // dummy data
-var books = [
+var book = [
     { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
     { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
@@ -12,7 +12,7 @@ const BookType = new GraphQLObjectType(
     {
         name: "Book",
         fields: () => ({
-            id: { type: GraphQLString },
+            id: { type: GraphQLID },
             name: { type: GraphQLString },
             genre: { type: GraphQLString }
         }),
@@ -25,7 +25,7 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: {id: {type:GraphQLString}},
+            args: {id: {type:GraphQLID}},
             resolve(parent, args) {
                 return _.find(book, {id: args.id})
             }
